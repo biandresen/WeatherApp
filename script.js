153765d39968f35df8e832e3a99536aa51fcd247
birando <birger1@live.com>
2024-08-11 10:27:17 +0200

    refactor

--- script.js
+++ script.js
@@ -1,62 +1,60 @@
-const searchButton = document.querySelector(".search-button");
-const tempButton = document.querySelector(".temperature-type-button");
-const locationInput = document.querySelector(".location-input");
-const locationResult = document.querySelector(".location-result");
-const conditionsResult = document.querySelector(".conditions-result");
-const temperatureResult = document.querySelector(".temperature-result");
-const gifArea = document.querySelector(".gif-area");
-const themeButton = document.querySelector(".color-theme-button");
+const searchButton = document.querySelector('.search-button');
+const tempButton = document.querySelector('.temperature-type-button');
+const locationInput = document.querySelector('.location-input');
+const locationResult = document.querySelector('.location-result');
+const conditionsResult = document.querySelector('.conditions-result');
+const temperatureResult = document.querySelector('.temperature-result');
+const gifArea = document.querySelector('.gif-area');
+const themeButton = document.querySelector('.color-theme-button');
 const root = document.documentElement;
-const img = document.createElement("img");
-let theme = "normal";
+const img = document.createElement('img');
+let theme = 'normal';
 let toggle = 0;
 
-searchButton.addEventListener("click", () => {
-  if (locationInput.value === "") return;
+searchButton.addEventListener('click', () => {
+  if (locationInput.value === '') return;
   else fetchWeatherData();
 });
-tempButton.addEventListener("click", convertTemperature);
-themeButton.addEventListener("click", changeTheme);
+tempButton.addEventListener('click', convertTemperature);
+themeButton.addEventListener('click', changeTheme);
 
 function changeTheme() {
-  if (root.style.getPropertyValue("--primary-color") === "#161B22") {
-    theme = "normal";
+  if (root.style.getPropertyValue('--primary-color') === '#161B22') {
+    theme = 'normal';
     setNormalTheme();
   } else {
-    theme = "dark";
+    theme = 'dark';
     setDarkTheme();
   }
 }
 
 function setNormalTheme() {
-  root.style.setProperty("--primary-color", "#0092b2");
-  root.style.setProperty("--secondary-color", "#c3f9ff");
-  root.style.setProperty("--detail-color", "#1a2529");
-  root.style.setProperty("--button-color", "#f69333");
-  root.style.setProperty("--text-color", "white");
-  root.style.setProperty("--background-color", "#5ec8ea");
+  root.style.setProperty('--primary-color', '#0092b2');
+  root.style.setProperty('--secondary-color', '#c3f9ff');
+  root.style.setProperty('--detail-color', '#1a2529');
+  root.style.setProperty('--button-color', '#f69333');
+  root.style.setProperty('--text-color', 'white');
+  root.style.setProperty('--background-color', '#5ec8ea');
 }
 
 function setDarkTheme() {
-  root.style.setProperty("--primary-color", "#161B22");
-  root.style.setProperty("--secondary-color", "#0D1117");
-  root.style.setProperty("--detail-color", "#FF7B72");
-  root.style.setProperty("--button-color", "#1a2529");
-  root.style.setProperty("--text-color", "#FFA657");
-  root.style.setProperty("--background-color", "#443b3e");
+  root.style.setProperty('--primary-color', '#161B22');
+  root.style.setProperty('--secondary-color', '#0D1117');
+  root.style.setProperty('--detail-color', '#FF7B72');
+  root.style.setProperty('--button-color', '#1a2529');
+  root.style.setProperty('--text-color', '#FFA657');
+  root.style.setProperty('--background-color', '#443b3e');
 }
 
 async function fetchWeatherData() {
   // WEATHER API - https://www.visualcrossing.com/
   removeGif();
   loadingData();
+  const location = locationInput.value.trim();
+  const endpoint = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=JX7LY9RYQCLWENELFC8FNQFWN`;
   try {
-    const location = locationInput.value.trim();
-    const response = await fetch(
-      "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
-        location +
-        "?key=JX7LY9RYQCLWENELFC8FNQFWN"
-    );
+    const response = await fetch(endpoint, { mode: 'cors' });
+    if (!response.ok) throw new Error(`City "${location}" not found`);
     const weatherInfo = await response.json();
     const weatherLocation = await weatherInfo.address.toUpperCase();
     const weatherConditions =
@@ -66,15 +64,15 @@ async function fetchWeatherData() {
     changeFontSize();
     fetchGif(weatherConditions);
   } catch (error) {
-    alert("Write a valid location");
+    alert(error);
     removeLoadingData();
   }
-  locationInput.value = "";
+  locationInput.value = '';
 }
 
 function loadingData() {
-  const loadingIcon = document.createElement("i");
-  loadingIcon.classList.add("fa-solid", "fa-spinner", "fa-2xl");
+  const loadingIcon = document.createElement('i');
+  loadingIcon.classList.add('fa-solid', 'fa-spinner', 'fa-2xl');
   gifArea.appendChild(loadingIcon);
 }
 
@@ -85,29 +83,29 @@ function removeLoadingData() {
 function displayWeatherData(weatherLocation, weatherConditions, weatherTemperature) {
   locationResult.textContent = weatherLocation;
   conditionsResult.textContent = weatherConditions;
-  temperatureResult.textContent = weatherTemperature.toFixed(0) + " ℉";
+  temperatureResult.textContent = weatherTemperature.toFixed(0) + ' ℉';
   toggle = 0;
 }
 
 function changeFontSize() {
   if (locationResult.textContent.length > 14) {
-    locationResult.style.fontSize = "1rem";
+    locationResult.style.fontSize = '1rem';
   }
   if (
     conditionsResult.textContent.length > 14 &&
     conditionsResult.textContent.length < 18
   ) {
-    conditionsResult.style.fontSize = "1rem";
+    conditionsResult.style.fontSize = '1rem';
   } else if (conditionsResult.textContent.length > 18) {
-    conditionsResult.style.fontSize = "0.9rem";
+    conditionsResult.style.fontSize = '0.9rem';
   }
 }
 
 function convertTemperature() {
-  if (temperatureResult.textContent === " ") return;
+  if (temperatureResult.textContent === ' ') return;
 
-  const celsius = " ℃";
-  const fahrenheit = " ℉";
+  const celsius = ' ℃';
+  const fahrenheit = ' ℉';
   const baseValue = Number(temperatureResult.textContent.slice(0, -2));
   if (toggle === 0) {
     const inCelsius = ((baseValue - 32) * (5 / 9)).toFixed(0);
@@ -123,19 +121,19 @@ function convertTemperature() {
 async function fetchGif(weatherConditions) {
   const searchItem = weatherConditions;
   let id;
-  if (searchItem.includes("PARTIALLY CLOUDY")) id = "KwZoSJlvep6Vy";
-  if (searchItem.includes("RAIN")) id = "l0HlPwMAzh13pcZ20";
-  if (searchItem.includes("SUNNY")) id = "o7R0zQ62m8Nk4";
-  if (searchItem.includes("CLEAR")) id = "kiCXF8mL3j6Oe0vAm9";
-  if (searchItem.includes("OVERCAST")) id = "dBXNPw0XBdF1n82BBf";
+  if (searchItem.includes('PARTIALLY CLOUDY')) id = 'KwZoSJlvep6Vy';
+  if (searchItem.includes('RAIN')) id = 'l0HlPwMAzh13pcZ20';
+  if (searchItem.includes('SUNNY')) id = 'o7R0zQ62m8Nk4';
+  if (searchItem.includes('CLEAR')) id = 'kiCXF8mL3j6Oe0vAm9';
+  if (searchItem.includes('OVERCAST')) id = 'dBXNPw0XBdF1n82BBf';
 
   const url =
-    "https://api.giphy.com/v1/gifs/" +
+    'https://api.giphy.com/v1/gifs/' +
     id +
-    "?api_key=hdnMHDSnCZzfPRcoMMlVx3Av33ROAJvz&s=";
+    '?api_key=hdnMHDSnCZzfPRcoMMlVx3Av33ROAJvz&s=';
 
   await fetch(url, {
-    mode: "cors",
+    mode: 'cors',
   })
     .then((response) => {
       return response.json();
